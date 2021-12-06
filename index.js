db4943209b545635f335a9432b637147a4f338a3
Yasir <yasir.ibrahim@hotmail.co.uk>
2021-12-06 16:45:17 +0000

    task 2c done

--- index.js
+++ index.js
@@ -15,5 +15,14 @@ let myCollection = [
       whatILike: "Eat. Sleep. Code. Repeat :)"
     }
   ];
-console.log(myCollection)
+//console.log(myCollection)
   
+function describeItem (item){
+if (item.count===1){
+  console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`)
+} else {
+  console.log(`I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`)
+}
+}
+
+describeItem(myCollection[1])

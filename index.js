b00d3f229c505885b8ecda10704964ff53b6d7ca
Yasir <yasir.ibrahim@hotmail.co.uk>
2021-12-07 11:58:31 +0000

    done

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
+import chalk from 'chalk';
+import myCollection from "./collection.js";
 //console.log(myCollection)
   
 function describeItem (item){
 if (item.count===1){
-  console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`)
+  console.log(`I have a ${chalk.cyan(item.name)}. Here's what I like about it: ${chalk.green(item.whatILike)}`)
 } else {
-  console.log(`I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`)
+  console.log(chalk.bgRed(`I have ${chalk.yellow(item.count)} ${chalk.cyan(item.name)}s. Here's what I like about them: ${chalk.green(item.whatILike)}`))
 }
 }
 
 describeItem(myCollection[1])
+
+function describeCollection(array){
+  for (let i=0; i<array.length; i++){
+     describeItem(array[i])
+  }
+}
+
+describeCollection(myCollection)

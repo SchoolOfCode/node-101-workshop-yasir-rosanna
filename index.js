6589ccc9e0cf52d74d7f808a5a3508618ae6802d
Rosanna D'Vaz <rosannadvaz@gmail.com>
2021-12-08 06:50:18 +0000

    task 2d done

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 let myCollection = [
-    {
-      name: "School of Code mug",
-      count: 1,
-      whatILike: "It has my cute pixel character on it!"
-    },
-    {
-      name: "School of Code hat",
-      count: 2,
-      whatILike: "An often overlooked fashion accessory"
-    },
-    {
-      name: "School of Code pillow",
-      count: 1,
-      whatILike: "Eat. Sleep. Code. Repeat :)"
-    }
-  ];
+  {
+    name: "School of Code mug",
+    count: 1,
+    whatILike: "It has my cute pixel character on it!",
+  },
+  {
+    name: "School of Code hat",
+    count: 2,
+    whatILike: "An often overlooked fashion accessory",
+  },
+  {
+    name: "School of Code pillow",
+    count: 1,
+    whatILike: "Eat. Sleep. Code. Repeat :)",
+  },
+];
 //console.log(myCollection)
-  
-function describeItem (item){
-if (item.count===1){
-  console.log(`I have a ${item.name}. Here's what I like about it: ${item.whatILike}`)
-} else {
-  console.log(`I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`)
-}
+
+function describeItem(item) {
+  if (item.count === 1) {
+    console.log(
+      `I have a ${item.name}. Here's what I like about it: ${item.whatILike}`
+    );
+  } else {
+    console.log(
+      `I have ${item.count} ${item.name}s. Here's what I like about them: ${item.whatILike}`
+    );
+  }
 }
 
-describeItem(myCollection[1])
+describeItem(myCollection[2]);
+
+function describeCollection(array){
+  for(let i = 0; i < array.length; i++){
+    describeCollection(array[i]);
+  }
+}
